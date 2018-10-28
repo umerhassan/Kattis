e0ed7e00b2eb5f37559359c12076cceb3e85de4e
umerhassan <33638819+umerhassan@users.noreply.github.com>
2018-10-28 12:25:54 -0600

    DRM Messages

--- src/DRMMessages.java
+++ src/DRMMessages.java
@@ -5,13 +5,34 @@ public class DRMMessages {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.next();
-		String output = "";
-		String[] arrOfStr = s.split("-", 100); 
-		for(int i = 0; i < arrOfStr.length;i++) {
-			char x=arrOfStr[i].charAt(0);
-			output = output + x;
+		int l = s.length();
+		StringBuilder part1 = new StringBuilder(s.substring(0, l/2));
+		StringBuilder part2 = new StringBuilder(s.substring(l/2, l));
+	//	String part2 = s.substring(l/2, l);
+	
+		int rotation1 = 0,rotation2=0;
+		for(int i = 0; i < part1.length();i++)
+			rotation1 = rotation1 + (int)part1.charAt(i) - 65;
+	
+		for(int i = 0; i < part2.length();i++)
+			rotation2 = rotation2 + (int)part2.charAt(i) - 65;
+		
+		for(int i = 0; i < part1.length();i++) {
+			char x = (char) (part1.charAt(i)+rotation1-65);
+			System.out.println(x);
+			part1.setCharAt(i, x);
+			if((int)part1.charAt(i) > 90) {
+				int diff =  ((int)part1.charAt(i) - 90);
+				char newX = (char)('A'+diff);
+				part1.setCharAt(i, newX);
+			}
 		}
-		System.out.println(output);
+			
+		
+		
+		System.out.println(part1);
+		System.out.println("\n");
+		System.out.println(part2);
 	
 	}
 
