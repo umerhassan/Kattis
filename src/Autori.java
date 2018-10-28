be90dff34a0842225d212be8bb9a021f76f40b78
umerhassan <33638819+umerhassan@users.noreply.github.com>
2018-10-28 00:16:59 -0600

    Autori

--- src/Autori.java
+++ src/Autori.java
@@ -5,15 +5,11 @@ public class Autori {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.next();
-		int output = 1;
-		for(int i = 0;i<s.length();i++) {
-			if(output == 1 && s.charAt(i) == 'A') output = 2;
-			else if(output == 2 && s.charAt(i) == 'A') output = 1;
-			if(output == 2 && s.charAt(i) == 'B') output = 3;
-			else if(output == 3 && s.charAt(i) == 'B') output = 2;
-			if(output == 1 && s.charAt(i) == 'C') output = 3;
-			else if(output == 3 && s.charAt(i) == 'C') output = 1;
-
+		String output = "";
+		String[] arrOfStr = s.split("-", 100); 
+		for(int i = 0; i < arrOfStr.length;i++) {
+			char x=arrOfStr[i].charAt(0);
+			output = output + x;
 		}
 		System.out.println(output);
 	
