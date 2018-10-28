526456f255ffd7f389fb5b154206c44ae99ef22e
umerhassan <33638819+umerhassan@users.noreply.github.com>
2018-10-27 22:23:51 -0600

    Tarifa Problem

--- src/Tarifa.java
+++ src/Tarifa.java
@@ -5,7 +5,10 @@ public class Tarifa {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int monthly = sc.nextInt();
-		
+		int N = sc.nextInt();
+		for(int i = 0;i<N;i++) {
+			
+		}
 		
 
 	}
